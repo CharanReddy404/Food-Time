a51c123e72c50ffeab7ea52af6d7a9a8913f030c
Charan <charanlucky6143@gmail.com>
2022-08-22 18:30:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const time = new Date().toLocaleTimeString("IT-it").slice(0, 2);
+    let output;
+    if (time < 12) {
+        output = "Breakfast Time";
+    } else if (time >= 12 && time < 18) {
+        output = "Lunch Time";
+    } else {
+        output = "Dinner Time";
+    }
+    return (
+        <div>
+            <h1 style={{ color: "black", backgroundColor: "yellow" }}>{output}</h1>
+        </div>
+    );
 }
 
 export default App;
